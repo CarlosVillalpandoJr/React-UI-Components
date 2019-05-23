4914a785f86b3eca819dd7446e72321428d67081
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-05-22 19:50:09 -0500

    added components to CardContainer

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './Card.css';
+import './CardBanner';
+import './CardContent';
 
 const CardContainer = () => {
     return (
         <div>
-
+            <CardBanner />
+            <CardContent />
         </div>
     )
 }
+
+export default CardContainer
