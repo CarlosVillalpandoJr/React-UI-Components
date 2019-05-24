8579b5790cc4795e0832fb7f2e75fee6aeaaea44
Carlos Villalpando, Jr <carlosvillalpandojr21@yahoo.com>
2019-05-24 17:04:00 -0500

    created ActionButton Component

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,10 @@
 import React from 'react';
 import './Button.css';
+
+const ActionButton = props => {
+    return (
+        <button>{props.display}</button>
+    )
+}
+
+export default ActionButton;
